20cbe8723b76d1b2275ffd48b7c99f0939a36e45
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-04-17 22:05:18 +0300

    pdf generator

--- packages/frontend/scaling_eth/app/components/pdfviewer.tsx
+++ packages/frontend/scaling_eth/app/components/pdfviewer.tsx
@@ -3,31 +3,38 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 import ReactPDF from '@react-pdf/renderer';
 
 
-// Create styles
+const lokiInfo = `
+Loki, Marvel Evreni'nde tanınan bir karakterdir. Tanrı Odin'in oğlu ve Thor'un kardeşidir. Genellikle yalanlar ve hilelerle tanınır. İskandinav mitolojisinde Loki, tanrı Odin'in kan kardeşidir ve daha çok hilebaz ve entrikacı olarak tasvir edilir. Marvel Sinematik Evreni'nde, Loki'yi Tom Hiddleston canlandırmaktadır.
+
+Loki'nin çeşitli güçleri ve yetenekleri vardır. Dönüşüm yeteneğiyle formunu değiştirebilir, telepati ve telekinezi gibi psionik güçlere sahiptir. Ayrıca, büyü yapma yeteneği ve son derece zeki olması da dikkat çekicidir. Ancak, karakterinin en belirgin özelliği belirsizlik ve karmaşıklıktır. Bazı zamanlarda kahraman olarak, bazı zamanlarda ise kötü niyetli bir düşman olarak karşımıza çıkar.
+
+Loki'nin Marvel Sinematik Evreni'ndeki popülerliği, onun kendi televizyon dizisi olan "Loki"nin yapılmasına neden oldu. Bu dizi, Loki'nin maceralarını ve çeşitli zaman çizgilerinde yaşadığı olayları konu almaktadır.
+`;
+
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+    backgroundColor: 'white'
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
+  container: {
+    padding: 40,
+    paddingTop: 20
+  },
+  lokiInfoText: {
+    fontSize: 12,
+    textAlign: 'justify'
   }
 });
 
-// Create Document Component
 const MyDocument = () => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
+      <View style={styles.container}>
+        <Text style={styles.lokiInfoText}>{lokiInfo}</Text>
       </View>
     </Page>
   </Document>
 );
-ReactPDF.renderToStream(<MyDocument />);
+
+
 export default MyDocument;
--- packages/frontend/scaling_eth/app/profile/page.tsx
+++ packages/frontend/scaling_eth/app/profile/page.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import NFTCard from '../components/NFTCard'
-import MyDocument from '../components/pdfviewer'
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
+import MyDocument from "../components/pdfviewer";
 
+// PDFDownloadLink'i dinamik olarak import ediyoruz
+const PDFDownloadLink = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFDownloadLink),
+  { ssr: false } // Bu, bileşenin sadece client tarafında render edilmesini sağlar
+);
 
-const profile = () => {
+const Profile = () => {
   return (
-    <div>
-      <MyDocument />
+    <div className="flex justify-center">
+      <PDFDownloadLink document={<MyDocument />} fileName="loki.pdf">
+        {({ loading }) =>
+          loading ? (
+            <button className="flex justify-center mt-6 h-[3.5rem] w-32 rounded-xl bg-white bg-opacity-80 text-black text-center items-center font-bold border border-black border-l-4 border-b-4">Loading Document...</button>
+          ) : (
+            <button className="flex justify-center mt-6 h-[3.5rem] w-32 rounded-xl bg-white bg-opacity-80 text-black text-center items-center font-bold border border-black border-l-4 border-b-4">
+              Download
+            </button>
+          )
+        }
+      </PDFDownloadLink>
     </div>
-  )
-}
+  );
+};
 
-export default profile
+export default Profile;
