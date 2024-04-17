4e74a0a1379c3029d61e8796b65cfd634aea065c
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-04-17 15:33:01 +0300

    react-pdf added

--- packages/frontend/scaling_eth/app/profile/page.tsx
+++ packages/frontend/scaling_eth/app/profile/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NFTCard from '../components/NFTCard'
+import MyDocument from '../components/pdfviewer'
+
 
 const profile = () => {
   return (
     <div>
-      
+      <MyDocument />
     </div>
   )
 }
